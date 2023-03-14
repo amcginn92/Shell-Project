82004f6f7ce59b1892111e0221ae3506fb753fe6
amcginn92 <tug74324@temple.edu>
2023-03-13 20:39:32 -0400

    Wrote some pseudocode for project execution. Created initial projectExec.c function to be included in the header/make files

--- myShell.c
+++ myShell.c
@@ -10,36 +10,39 @@ int cd(char** arr, int i);
 void pwd();
 //==================================
 int main(int argc, char* argv[]){
-
-	char _line[1024];
-	char** arr = NULL;
+	char cwd[4096];	//used to display cwd to user in prompt
+	char _line[1024]; //Input from user
+	char** arr = NULL;	//parsed input values separated into strings
 	char* line = NULL;
 	//continue taking input from user until exit is given
-	int j = 1;
+//	int j = 1;
 	while(1){
-		printf("myshell-client:~/cwd%d> ", j);	//would be cool if we had cwd here
+		printf("myshell-client:~%s>", getcwd(cwd, 4096));	//would be cool if we had cwd here
 		fgets(_line,1024, stdin);
 
-		line = strdup(_line);	//We create a copy so parse doesn't change our arguments
+		line = strdup(_line);
 		arr = parse(line," \n");
 
-		if (arr==NULL)
+		if (arr==NULL){
 			exit(1);
-
+		}
+		//get a count of arguments
 		int i = 0;
 		while (arr[i]!=NULL){
 //			printf("arr[%d]: ", i);
 //			printf("%s\n", arr[i++]);
 			i++;
 		}
-//		printf("%d\n", i);
-
-		if(i > 2){	//we should not have more than 2 arguments
+		//determine if input was a built in
+		if(i > 2){	//we should not have more than 2 arguments for built ins
 			printf("Too many arguments for now\n");
 			exit(1);
 		}
-
 		runBuiltIn(arr, i);
+
+		//if it is not a built-in, run program
+		//programExec();
+
 
 
 		free(arr);
@@ -53,6 +56,8 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 //=====================================Functions
+
+//====================================================RUN BUILT INS
 int runBuiltIn(char** arr, int i){
 	if(strcmp(arr[0], "exit") == 0){
 		exit(0);
@@ -71,7 +76,7 @@ int runBuiltIn(char** arr, int i){
 	}
 	return(1);
 }
-
+//====================================================HELP
 void help(char** arr, int i){
 	if(i == 1){
 		helpShell();
@@ -88,10 +93,12 @@ void help(char** arr, int i){
 		//implement helpWait()
 	}
 }
+//====================================================PWD
 void pwd(){
-	char arr[1024];
-	printf("%s\n", getcwd(arr,1024));
+	char arr[4096];
+	printf("%s\n", getcwd(arr,4096));
 }
+//====================================================CD
 int cd(char** arr, int i){
 	if(i == 1){
 		return(0);	//cd was only argument, do nothing
